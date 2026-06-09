6f9d875a642d0738def6b3203016e7aece9ec16b
unknown <jxh1482@student.bham.ac.uk>
2026-06-09 17:55:36 +0100

    Basic Cell/Grid Objects 2

--- backend/maze_algorithms.py
+++ backend/maze_algorithms.py
@@ -7,6 +7,31 @@ class Cell():
         self.paths = paths
         self.is_visited = is_visited
 
+    # Get the Cell's Paths
+    def get_paths(self) -> set:
+        return self.paths
+    
+    # Set the Cell's Paths with New Path Set
+    def set_paths(self, paths: set) -> None:
+        self.paths = paths
+
+    # Set an Individual Cell Path as 'True' (Connected) or 'False' (Not Connected)
+    def set_path(self, path: chr, is_path: bool) -> None:
+        # Add Path when Absent
+        if (is_path) and (path not in self.paths):
+            self.paths.add(path)
+        # Remove Path when Present
+        elif (not is_path) and (path in self.paths):
+            self.paths.discard(path)
+
+    # Get Cell's "Visited Status"
+    def get_visited(self) -> bool:
+        return self.is_visited
+    
+    # Set Cell's "Visited Status" as Visited
+    def set_visited(self) -> None:
+        self.is_visited = True
+
 # Grid Object
 class Grid():
     def __init__(self, height: int, width: int, is_open: bool = False):
@@ -42,6 +67,19 @@ class Grid():
             cell_grid.append(row)
         self.cell_grid = cell_grid
 
-def test_output(algorithm: str, seed: str = "No Seed"):
+    # Get the Grid's Dimensions
+    def get_dimensions(self) -> tuple:
+        return (self.height, self.width)
+    
+    # Get a Grid's Cell via Coordinates
+    def get_cell(self, y: int, x: int) -> Cell:
+        return self.cell_grid[y][x]
+    
+    # Set a Grid's Cell via Coordinates
+    def set_cell(self, y: int, x: int, cell: Cell) -> None:
+        self.cell_grid[y][x] = cell
+
+# Test Response
+def test_output(algorithm: str, seed: str = "No Seed") -> str:
     return f"Maze w/ {algorithm}, Seeded w/ {seed}"
 
