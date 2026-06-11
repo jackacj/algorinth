6fef2f0275992cbf17876316de97f98a2aa32c03
unknown <jxh1482@student.bham.ac.uk>
2026-06-11 13:52:09 +0100

    Initial Frontened Blockout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,5 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 export default function App() {
-  return (<p> Blank Frontend </p>);
+  return (
+    <div class="container">
+      <p class="debugMarker"> Title Pane </p>
+      <h1> Untitled Maze Generation Project </h1>
+      <div class="container">
+        <p class="debugMarker"> Main Pane </p>
+        <div class="container">
+          <p class="debugMarker"> Maze Pane </p>
+          <div class="container">
+            <p class="debugMarker"> Playback Pane </p>
+          </div>
+        </div>
+        <div class="container">
+          <p class="debugMarker"> Config Pane </p>
+        </div>
+      </div>
+    </div>
+  );
 }
