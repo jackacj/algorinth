08e81b09d77864f19cfb294d264d85303156c958
unknown <jxh1482@student.bham.ac.uk>
2026-06-11 16:22:42 +0100

    Added Conditional Rendering to Cells for Paths & Visited Status

--- frontend/src/components/maze/Cell.jsx
+++ frontend/src/components/maze/Cell.jsx
@@ -1,10 +1,21 @@
 import '../../styles/Cell.css'
 
-export default function Cell({ row, col }) {
+export default function Cell({ cell }) {
+    // Unpack Cell Data
+    const { row, col, visited, paths } = cell
 
     return(
-        <div className="cell">
-            {/* <p className="cellPosition">{row},{col}</p> */}
-        </div>
+        <div 
+            className="cell"
+            style={{
+                // Render Border when No Path Exists
+                "borderTop": paths.north ? "none" : "2px solid",
+                "borderBottom": paths.south ? "none" : "2px solid",
+                "borderRight": paths.east ? "none" : "2px solid",
+                "borderLeft": paths.west ? "none" : "2px solid",
+                // Highlight Cell when Visited
+                "backgroundColor": visited ? "lightskyblue" : "white"
+            }} 
+        />
     );
 }
--- frontend/src/components/maze/Grid.jsx
+++ frontend/src/components/maze/Grid.jsx
@@ -1,18 +1,49 @@
 import '../../styles/Grid.css'
 import Cell from './Cell';
 
-export default function Grid({ rows, cols }) {
-    const totalCells = rows * cols;
+// Create Default (Closed) Grid from Dimensions
+function createGrid(rows, cols) {
+    const grid = []
 
-    // Return CSS Grid
+    // For Each Row...
+    for (let row = 0; row < rows; row++) {
+        const currentRow = [];
+
+        // For Each Column... (Individual Cell)
+        for (let col = 0; col < cols; col++) {
+            // Add Cell Data to (Row,Col) Position in Current Row
+            currentRow.push({
+                row,
+                col,
+                visited: false,
+                paths: {
+                    north: false,
+                    south: false,
+                    east: false,
+                    west: false
+                }
+            });
+        }
+
+        // Add Current Row to Grid
+        grid.push(currentRow)
+    }
+
+    return grid
+}
+
+export default function Grid({ rows, cols }) {
+    // Create Default (Closed) Grid
+    const grid = createGrid(rows, cols);
+
+    // Return Grid, Visually Arranged into Columns via CSS
     return(
         <div className="grid" style={{ gridTemplateColumns: `repeat(${cols}, 25px)` }}>
             { // Programmatically Create Grid Cells
-            Array.from({ length: rows }).map((_, row) =>
-                Array.from({ length: cols }).map((_, col) => (
+            grid.map((row, rowIdx) =>
+                row.map((cell, cellIdx) => (
                     <Cell
-                        row={row}
-                        col={col}
+                        cell={cell}
                     />
                 ))
             )}
