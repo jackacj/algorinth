04fcc36cb0bb997678394463bd591569946027e5
unknown <jxh1482@student.bham.ac.uk>
2026-07-02 22:24:10 +0100

    Updated Project Name

--- frontend/src/components/layout/TitlePane.jsx
+++ frontend/src/components/layout/TitlePane.jsx
@@ -5,7 +5,8 @@ export default function TitlePane(){
     return (
         <div id="titlePane" className="container">
             <p className="debugMarker"> Title Pane </p>
-            <h1> Untitled Maze Generation Project </h1>
+            <h1> Algorinth </h1>
+            <h3> by Jack Hartley, 2026</h3>
         </div>
     );
 }
