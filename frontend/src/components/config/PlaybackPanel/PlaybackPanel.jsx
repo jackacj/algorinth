05dd929f812bca765253ddff9c89cc20e7836b55
jackacj <jack.acj.hartley2@gmail.com>
2026-07-17 00:26:05 +0100

    Improved Playback Button Styling + Recolouring Grid & Drop Shadows

--- frontend/src/components/config/PlaybackPanel/PlaybackPanel.jsx
+++ frontend/src/components/config/PlaybackPanel/PlaybackPanel.jsx
@@ -35,42 +35,52 @@ export default function PlaybackPanel({ playback, onPressPlayback, onSpeedChange
         <div id="playbackPanel" className="panel">
             {/* Playback Buttons */}
             {/* Step Forward Button -> Disabled when Autoplaying */}
-            <button class="playbackButton"
+            <button className="playbackButton pushableButton"
                 onClick={() => onPressPlayback("STEP_FORWARD")}
                 disabled={playback.isAuto}
             >
-                <FontAwesomeIcon icon={faForwardStep} />
+                <span className="playbackButtonFront pushableButtonFront">
+                    <FontAwesomeIcon icon={faForwardStep} />
+                </span>
             </button>
             {/* Step Backward Button -> Disabled when Autoplaying */}
-            <button class="playbackButton"
+            <button className="playbackButton pushableButton"
                 onClick={() => onPressPlayback("STEP_BACKWARD")}
                 disabled={playback.isAuto}
             >
-                <FontAwesomeIcon icon={faBackwardStep} />
+                <span className="playbackButtonFront pushableButtonFront">
+                    <FontAwesomeIcon icon={faBackwardStep} />
+                </span>
             </button>
             {/* Autostep Forward -> Replaced w/ Speed Controller when Autoplaying Forward */}
             {playback.isAuto && playback.isAutoForward && (speedController)}
             {(!playback.isAuto || !playback.isAutoForward) && (
-                <button class="playbackButton"
+                <button class="playbackButton pushableButton"
                     onClick={() => onPressPlayback("AUTOSTEP_PLAY_FORWARD")}
                 >
-                    <FontAwesomeIcon icon={faForwardFast} />
+                    <span className="playbackButtonFront pushableButtonFront">
+                        <FontAwesomeIcon icon={faForwardFast} />
+                    </span>
                 </button>
             )}
             {/* Autostep Backward -> Replaced w/ Speed Controller when Autoplaying Backward */}
             {playback.isAuto && !playback.isAutoForward && (speedController)}
             {(!playback.isAuto || playback.isAutoForward) && (
-                <button class="playbackButton"
+                <button class="playbackButton pushableButton"
                     onClick={() => onPressPlayback("AUTOSTEP_PLAY_BACKWARD")}
                 >
-                    <FontAwesomeIcon icon={faBackwardFast} />
+                    <span className="playbackButtonFront pushableButtonFront">
+                        <FontAwesomeIcon icon={faBackwardFast} />
+                   </span>
                 </button>
             )}
-            <button class="playbackButton"
+            <button class="playbackButton pushableButton"
                 disabled={!playback.isAuto}
                 onClick={() => onPressPlayback("AUTOSTEP_PAUSE")}
             >
-                <FontAwesomeIcon icon={faPause} />
+                <span className="playbackButtonFront pushableButtonFront">
+                    <FontAwesomeIcon icon={faPause} />
+                </span>
             </button>
             {/* Step Counter */}
             <div id="stepCounter" className="panel">
--- frontend/src/components/maze/Cell/Cell.jsx
+++ frontend/src/components/maze/Cell/Cell.jsx
@@ -9,12 +9,12 @@ export default function Cell({ cell }) {
             className="cell"
             style={{
                 // Render Border when No Path Exists
-                "borderTop": paths.north ? "none" : "2px solid black",
-                "borderBottom": paths.south ? "none" : "2px solid black",
-                "borderRight": paths.east ? "none" : "2px solid black",
-                "borderLeft": paths.west ? "none" : "2px solid black",
+                "borderTop": paths.north ? "none" : "2px solid var(--colour-cell-border)",
+                "borderBottom": paths.south ? "none" : "2px solid var(--colour-cell-border)",
+                "borderRight": paths.east ? "none" : "2px solid var(--colour-cell-border)",
+                "borderLeft": paths.west ? "none" : "2px solid var(--colour-cell-border)",
                 // Highlight Cell when Visited
-                "backgroundColor": visited ? "lightskyblue" : "white"
+                "backgroundColor": visited ? "var(--colour-cell-visited)" : "var(--colour-cell-unvisited)"
             }} 
         />
     );
